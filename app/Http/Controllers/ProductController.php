145625338b65eb92363f3c7ebd5fe4f2a53b6bd3
Ramadhan Zakki <ramadhanzakki506@gmail.com>
2026-09-21 16:23:55 +0700

    feat: created delete controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -93,6 +93,12 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        if ($product->image) {
+            Storage::disk('public')->delete($product->image);
+        }
+
+        $product->delete();
+
+        return redirect()->route('products.index');
     }
 }
