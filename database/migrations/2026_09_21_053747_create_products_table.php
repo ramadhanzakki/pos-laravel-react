b1676b6a77caf4029d3acdb072e2dedd602869f9
Ramadhan Zakki <ramadhanzakki506@gmail.com>
2026-09-21 15:46:14 +0700

    feat: created database migration

--- database/migrations/2026_09_21_053747_create_products_table.php
+++ database/migrations/2026_09_21_053747_create_products_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("category_id")->constrained()->cascadeOnDelete();
+            $table->string("name");
+            $table->text("description")->nullable();
+            $table->decimal("price", 10, 2);
+            $table->integer("stock")->default(0);
+            $table->string("image")->nullable();
+            $table->boolean("is_active")->default(true);
             $table->timestamps();
         });
     }
