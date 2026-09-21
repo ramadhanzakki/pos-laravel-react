46d6f2f9205a2a70491e1087758b34d135c08d43
Ramadhan Zakki <ramadhanzakki506@gmail.com>
2026-09-21 16:29:50 +0700

    feat: added product route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+    Route::resource('products', ProductController::class)->except('edit','create', 'show');
 });
 
 require __DIR__.'/settings.php';
