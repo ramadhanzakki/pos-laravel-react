6faf2b811e2f8cf3a90b8014b741a90d1493f7ba
Ramadhan Zakki <ramadhanzakki506@gmail.com>
2026-09-21 15:54:54 +0700

    feat: created models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
+#[Fillable(
+    "name",
+    "description"
+)]
 class Category extends Model
 {
-    //
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Override;
 
+#[Fillable(
+    "category_id",
+    "name",
+    "description",
+    "price",
+    "stock",
+    "image",
+    "is_active"
+)]
 class Product extends Model
 {
-    //
+    #[Override]
+    protected function casts()
+    {
+        return [
+            "price" => "decimal:2",
+            "is_active" => "boolean"
+        ];
+    }
+
+    public function categories(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
